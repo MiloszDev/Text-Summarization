19d4ada69da1ff951b71615dc30500c5ee64f1a3
Milosz <milosz.zawolik@gmail.com>
2025-03-10 19:00:46 +0100

    Added small changes.

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
+import boto3
 import uvicorn
+
 from fastapi import FastAPI
 from starlette.responses import Response
 from starlette.responses import RedirectResponse
 from src.stages.Prediction import PredictionPipeline
 
+AWS_REGION = 'eu-north-1'
 
 app = FastAPI()
 
+def get_ssl_credentials():
+    ssm = boto3.client('ssm', region_name=AWS_REGION)
+
+    cert = ssm.get_parameter(Name='/cert', WithDecryption=True)['Parameter']['Value']
+    key = ssm.get_parameter(Name='/key', WithDecryption=True)['Parameter']['Value']
+
+    with open("/tmp/cert.pem", "w") as cert_file:
+        cert_file.write(cert)
+
+    with open("/tmp/key.pem", "w") as key_file:
+        key_file.write(key)
+
+    return "/tmp/cert.pem", "/tmp/key.pem"
 
 @app.get("/", tags=["authentication"])
 async def index():
@@ -34,10 +50,12 @@ async def predict_route(text: str):
 
 
 if __name__ == "__main__":
+    cert_path, key_path = get_ssl_credentials()
+
     uvicorn.run(
         app, 
         host="0.0.0.0", 
         port=8080, 
-        ssl_keyfile="key.pem", 
-        ssl_certfile="cert.pem"
+        ssl_keyfile=key_path, 
+        ssl_certfile=cert_path
     )
